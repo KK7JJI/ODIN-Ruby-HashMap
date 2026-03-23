2510e40db061e58ddca93cb6e60feee9e1e4e903
kk7jji <kk7jji@outlook.com>
2026-03-23 10:35:10 -0700

    add a hash method.
    The hash method is injected allowing for alternate
    hash functions to be used without changing the
    class.

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
 require_relative 'hashmap/hashmap'
+
+hash_func = lambda do |key|
+  hash_code = 0
+  prime_number = 31
+
+  key.each_char { |char| hash_code = (prime_number * hash_code) + char.ord }
+
+  hash_code
+end
+
+hashmap = HashMap::HashMap.new(hash_func: hash_func)
+
+puts hashmap.hash('a')
+
+hashmap = HashMap::HashMap.new
+
+puts hashmap.hash('a')
--- lib/hashmap/hashmap.rb
+++ lib/hashmap/hashmap.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
-module Hash
+module HashMap
   class HashMap
     attr_accessor :length
+    attr_reader :hash_func
 
-    def initialize
+    def initialize(hash_func: HashFuncs.new.method(:hash_func))
+      @hash_func = hash_func
       @length = 0
+    end
+
+    def hash(key)
+      raise NoMethodError, 'Hash function not defined' unless @hash_func
+
+      hash_func.call(key) if @hash_func
+    end
+  end
+
+  class HashFuncs
+    def hash_func(key)
+      hash_code = 0
+      prime_number = 31
+
+      key.each_char { |char| hash_code = (prime_number * hash_code) + char.ord }
+
+      hash_code
     end
   end
 end
