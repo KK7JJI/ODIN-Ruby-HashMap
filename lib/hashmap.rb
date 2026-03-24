31d94e0a3de5b8c138474fa248fb05429fd42f4f
kk7jji <kk7jji@outlook.com>
2026-03-23 23:02:35 -0700

    test runs of the hash.
    I started it on a fairly big file of words
    and it was quite clear that runtime would
    be very long.  The largest set tested thus
    far is 102 elements which expands the hash
    table from 16 to 256.

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -3,19 +3,29 @@
 require_relative 'hashmap/hashmap'
 require_relative 'hashmap/linkedlist'
 
-hash_func = lambda do |key|
-  hash_code = 0
-  prime_number = 31
+REWORDS = /[a-zA-Z]+/
 
-  key.each_char { |char| hash_code = (prime_number * hash_code) + char.ord }
+hm = HashMap::HashMap.new
 
-  hash_code
+words = []
+File.readlines('./hashmap/hashmap.rb').each do |line|
+  line.scan(REWORDS).each do |word|
+    hm.set(key: word, value: word.upcase)
+  end
 end
 
-hashmap = HashMap::HashMap.new(hash_func: hash_func)
+File.readlines('./hashmap/linkedlist.rb').each do |line|
+  line.scan(REWORDS).each do |word|
+    hm.set(key: word, value: word.upcase)
+  end
+end
 
-puts hashmap.hash('a')
+puts hm.length
 
-hashmap = HashMap::HashMap.new
+puts hm.has?('current')
+puts hm.remove(key: 'current')
+puts hm.has?('current')
 
-puts hashmap.hash('a')
+puts hm.keys.inspect
+puts hm.values.inspect
+puts hm.entries.inspect
